e4c6694af80249005a45e3ec8d08a07ea9da214c
Varun <103885330+V-sao@users.noreply.github.com>
2024-02-15 01:20:28 +0530

    Add files via upload

---
+++ Pattern/src/Half_dimond.java
@@ -0,0 +1,25 @@
+
+public class Half_dimond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+			
+		}
+		for(int i=1;i<=4;i++) {
+			for (int j=i;j<=4;j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+			
+		}
+		
+	}
+	
+
+}
---
+++ Pattern/src/Spaced_Rightangled.java
@@ -0,0 +1,18 @@
+
+public class Spaced_Rightangled {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++) {
+			for(int j=i;j<=4;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+
+}
